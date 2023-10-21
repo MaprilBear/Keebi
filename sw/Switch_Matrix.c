a981a21e5bd76a99a4ef98a0ca7e0c892d5f79bf
April <maprilbear@gmail.com>
2023-10-21 13:53:00 -0500

    small cleanup

--- sw/Switch_Matrix.c
+++ sw/Switch_Matrix.c
@@ -24,8 +24,9 @@ void Switch_Handler(){
 	
 			// ROW0 - PF0
 			
+			/*
 			// pulse row
-			GPIO_PORTF_DATA_R |= 0x1;
+			GPIO_PORTF_DATA_R |= 0x2;
 			Clock_Delay(100);
 			
 			// sense columns
@@ -76,14 +77,16 @@ void Switch_Handler(){
 			
 			lastRow0 = currentRow0;
 			
+			
 			GPIO_PORTF_DATA_R = 0;
 			Clock_Delay(100);
 			
+			*/
 			// ROW1 - PF1
 			
 			// pulse row
-			GPIO_PORTF_DATA_R |= 0x2;
-			Clock_Delay(100);
+			GPIO_PORTF_DATA_R |= 0x4;
+			Clock_Delay(10);
 			
 			// sense columns
 			if (GPIO_PORTB_DATA_R & (1 << 4)){
@@ -134,12 +137,12 @@ void Switch_Handler(){
 			lastRow1 = currentRow1;
 			
 			GPIO_PORTF_DATA_R = 0;
-			Clock_Delay(100);
+			//Clock_Delay(100);
 			
 			// ROW2 - PF2
-			
+			/*
 			// pulse row
-			GPIO_PORTF_DATA_R |= 0x4;
+			GPIO_PORTF_DATA_R |= 0x8;
 			Clock_Delay(100);
 			
 			// sense columns
@@ -193,7 +196,7 @@ void Switch_Handler(){
 	
 			GPIO_PORTF_DATA_R = 0;
 			Clock_Delay(100);
-	
+	*/
 }
 
 
@@ -202,14 +205,13 @@ void Switch_Init(){
 	SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R1 | SYSCTL_RCGCGPIO_R5; // Activate port B and F
 	while ((SYSCTL_PRGPIO_R & (SYSCTL_PRGPIO_R1 | SYSCTL_PRGPIO_R5)) != (SYSCTL_PRGPIO_R1 | SYSCTL_PRGPIO_R5)){}
 	
-	GPIO_PORTF_DIR_R |= 1 + (1 << 1) + (1 << 2) + (1<<3); // Set rows to output
+	GPIO_PORTF_DIR_R |= (1 << 1) + (1 << 2) + (1 << 3); // Set rows to output
 	GPIO_PORTF_DEN_R |= 1 + (1 << 1) + (1 << 2) + (1<<3);
 	
 	GPIO_PORTB_DEN_R |= (1 << 4) + (1 << 5) + (1 << 6);
 	
-	GPIO_PORTF_DATA_R |= 0x8;
 	
-	Timer2A_Init(&Switch_Handler, 800000, 4); // Poll at 100 Hz (3 cols)
+	Timer2A_Init(&Switch_Handler, 8000, 4); // Poll at 100 Hz (3 cols)
 }
 
 void Switch_Stop(){
